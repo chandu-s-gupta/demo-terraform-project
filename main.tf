019af4c9ddba720295ca21cc9825ed6cad0430b9
Chandrashekhar Gupta <38191193+chandu-s-gupta@users.noreply.github.com>
2025-04-14 12:41:40 +0530

    Create main.tf
    shld be in local_state repo

---
+++ main.tf
@@ -0,0 +1,54 @@
+#local_state
+
+terraform {
+  backend "s3"{
+   bucket  = "160382898665-terraform-states"
+   key     = "development/service-name.tfstate"
+   encrypt = true
+   region  = "us-west-2"
+   dynamodb_table = "terraform-lock"
+
+   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
+
